48351d88227e81daef655ecda46ee95cdd045b4e
who <nadezhda.bn.02@mail.ru>
2023-06-08 20:19:16 +0300

    ok

--- SocialNetworksImages/SocialNetworksImages/Services/ApiVK/ConnectorVK.cs
+++ SocialNetworksImages/SocialNetworksImages/Services/ApiVK/ConnectorVK.cs
@@ -4,9 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SocialNetworksImages.Services.ApiVK
+namespace SocialNetworksImages.Services.ApiVK;
+
+public class ConnectorVK
 {
-    internal class ConnectorVK
-    {
-    }
+    string AppId = "51668994";
+    string AppSecret = "mIA8j9BzkdPQhHskqQHt"; //secret key
+    string ServiceKey = "7106264f7106264f7106264fa372124e4d771067106264f157944981ec917c478c52cc5";
+    string AutorizationEndpoint = "";
+    string TokenEndPoint = "";
 }
--- SocialNetworksImages/SocialNetworksImages/ViewModel/MainWindow.xaml.cs
+++ SocialNetworksImages/SocialNetworksImages/ViewModel/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SocialNetworksImages.Services.ApiVK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,5 +25,8 @@ namespace SocialNetworksImages
         {
             InitializeComponent();
         }
+
+
+
     }
 }
